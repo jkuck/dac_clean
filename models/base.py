0edf78ec1aabaff1c071cbdc7cdb5301bf34dfb2
jkuck <jkuck@lyft.com>
2019-11-04 20:44:11 +0000

    distance loss, no FP exp

--- models/base.py
+++ models/base.py
@@ -71,8 +71,8 @@ def compute_filter_loss_distance(logits, labels, pred_cluster, gt_objects, weigh
     assert((batch_size, gt_obj_count) == distances.shape), (batch_size, gt_obj_count, distances.shape)
 
 
-    loss = lamb * bcent[:,:-1] + distances #exclude FP class
-    # loss = lamb * bcent + distances
+#     loss = lamb * bcent[:,:-1] + distances #exclude FP class
+    loss = lamb * bcent + distances
     # print("loss:", loss[0])
     
 
